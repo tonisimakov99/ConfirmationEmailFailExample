34b336b482cf60cd9299b4e5f6f5e09f7d41c33a
anton simakov <toni.simakov.99@bk.ru>
2022-07-29 16:02:08 +0500

    test

---
+++ testconsole/Program.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Data.Sqlite;
+
+namespace TestConsole
+{
+    public class TestIdentityDbContext : IdentityDbContext<IdentityUser>
+    {
+        /// <summary>
+        /// Создавет контекст
+        /// </summary>
+        /// <param name="options"></param>
+        public TestIdentityDbContext(DbContextOptions<TestIdentityDbContext> options)
+          : base(options)
+        {
+
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            Start().Wait();
+            Console.WriteLine();
+        }
+        private static async Task Start()
+        {
+            var services = new ServiceCollection();
+            services.AddLogging();
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+               {
+                   options.User.RequireUniqueEmail = true;
+                   options.Password.RequireNonAlphanumeric = false;
+               }).AddDefaultTokenProviders()
+               .AddEntityFrameworkStores<TestIdentityDbContext>();
+             services.AddScoped(t =>
+                {
+                    var connection = new SqliteConnection("Filename=:memory:");
+                    connection.Open();
+                    var contextOptions = new DbContextOptionsBuilder<TestIdentityDbContext>()
+                        .UseSqlite(connection)
+                        .Options;
+                    var dbContext = new TestIdentityDbContext(contextOptions);
+                    dbContext.Database.EnsureCreated();
+                    return dbContext;
+                });
+            var provider = services.BuildServiceProvider();
+            var userManager = provider.GetRequiredService<UserManager<IdentityUser>>();
+            await userManager.CreateAsync(new IdentityUser("userName")
+            {
+                Email = "userName@test.ru",
+                PhoneNumberConfirmed = false,
+                EmailConfirmed = false
+            });
+            var user = await userManager.FindByEmailAsync("userName@test.ru");
+
+            var emailToken = await userManager.GenerateEmailConfirmationTokenAsync(user);
+            var phoneToken = await userManager.GenerateChangePhoneNumberTokenAsync(user, "88888888888");
+            var phoneChangeResult = await userManager.ChangePhoneNumberAsync(user, "88888888888", phoneToken);
+            var emailConfirmResult = await userManager.ConfirmEmailAsync(user, emailToken);
+            Console.WriteLine(phoneChangeResult);
+            Console.WriteLine(emailConfirmResult);
+        }
+    }
+}
---
+++ testconsole/RegisterModel.cs
@@ -0,0 +1,87 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Authorization.DbModels
+{
+    /// <summary>
+    /// Модель данных для регистрации пользователя
+    /// </summary>
+    public class RegisterModel
+    {
+        /// <summary>
+        /// Создает модель данных регистрации
+        /// </summary>
+        /// <param name="userName">логин пользователя</param>
+        /// <param name="email">email пользователя</param>
+        /// <param name="password">пароль пользователя</param>
+        /// <param name="confirmPassword"> пароль для подтверждения</param>
+        /// <param name="firstName">имя пользователя</param>
+        /// <param name="lastName"> фамиля пользователя</param>
+        /// <param name="city">город пользователя</param>
+        /// <param name="phoneNumber"></param>
+        public RegisterModel(string userName, string email, string password, string confirmPassword, string firstName, string lastName, string city, string phoneNumber)
+        {
+            UserName = userName;
+            Email = email;
+            Password = password;
+            ConfirmPassword = confirmPassword;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+            PhoneNumber = phoneNumber;
+        }
+
+        /// <summary>
+        /// Логин пользователя
+        /// </summary>
+        [Required]
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Email пользователя
+        /// </summary>
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Пароль пользователя
+        /// </summary>
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Пароль для подтверждения
+        /// </summary>
+        [Required]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        /// <summary>
+        /// Имф пользователя
+        /// </summary>
+        [Required]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Фамилия пользователя
+        /// </summary>
+        [Required]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Город пользователя
+        /// </summary>
+        [Required]
+        public string City { get; set; }
+
+        /// <summary>
+        /// Номер телефона
+        /// </summary>
+        /// <value></value>
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; }
+    }
+}
